9014356b7b3ad8fbb3d5a70b21d9e9a292f8d8eb
Surya Kanteti <suryateja.kanteti@gmail.com>
2022-10-02 22:44:11 -0600

    Added commit number for the updated server

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 531280c6d123be8572b81638bb3f1b2b9a0b8147
+AESD_ASSIGNMENTS_VERSION = 8f2e7cd8848390689b7b669ef81b08c960883fc8
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
