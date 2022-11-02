4a4f7c36b9a047e38c355c25a91e2f14a3cdc98f
Surya Kanteti <suryateja.kanteti@gmail.com>
2022-11-01 20:45:43 -0600

    Working full test

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 8f2e7cd8848390689b7b669ef81b08c960883fc8
+AESD_ASSIGNMENTS_VERSION = c4862aab8f81f3babe6ce718afc54f6227f0b48a
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
